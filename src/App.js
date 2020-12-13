3c66aa3b81b287ba3fb263bbc92ff585640ef86e
Rinat Sharifullin <74192788+flipdishRinat@users.noreply.github.com>
2020-12-13 18:47:46 +0000

    Radio working

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
 import axios from "axios";
 
-import Player from "./Player";
 import { useEffect, useState } from "react";
 
-// const playMe = (url) => {
-// try {
-//   var audio = new Audio(url);
-//   audio.play();
-// } catch (error) {
-//   console.error(error);
-// }
-//   return audio.isPlaying;
-// };
-
-// GetRussianSatations();
 function App() {
   const [stations, setStations] = useState([]);
-  // const stations = GetRussianSatations();
-  // console.log(stations);
 
   const GetRussianSatations = async () => {
     try {
       const response = await axios.request(
         "http://all.api.radio-browser.info/json/stations/bycountry/russia"
       );
-      // var par = JSON.parse(response.data);
-      // console.log(response.data);
-      // console.log(response.data[0].url);
       setStations(response.data);
-      console.log("GetRussianSatations call");
-      // return response.data;
     } catch (e) {
       console.log(e);
     }
@@ -39,28 +18,44 @@ function App() {
   useEffect(() => {
     GetRussianSatations();
   }, []);
-  // console.log(stations.keys.url);
+
+  var audio = new Audio();
+
+  const playMe = (e) => {
+    // const myUrl = stations.filter((item) => e.target.innerHTML === item.name);
+    console.log(e.target.id);
+    audio.pause();
+    audio = new Audio(e.target.id);
+    audio.play();
+  };
+
+  const stopMe = () => {
+    audio.pause();
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <Player url="http://ic4.101.ru:8000/stream/air/aac/64/200" />
-        {/* <a>{stations}</a> */}
-        <a>Privet</a>
+        <h1>World Web Radio</h1>
         <ul>
           {stations.map((item) => (
-            <li>{item.url}</li>
+            <li>
+              <button id={item.url_resolved} onClick={playMe}>
+                {item.name}
+              </button>
+              <button onClick={stopMe}>Stop</button>
+              <img
+                src={item.favicon}
+                width="50"
+                heigth="50"
+                alt={item.name}
+              ></img>
+              <p>Bitrate - {item.bitrate}</p>
+              <a href={item.homepage} target="_blank">
+                Home page
+              </a>
+            </li>
           ))}
         </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
