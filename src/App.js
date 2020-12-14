30b85d58323fb56e90cff667fd6c9f9585b14e4d
Rinat Sharifullin <74192788+flipdishRinat@users.noreply.github.com>
2020-12-14 07:52:35 +0000

    Add now playing

--- src/App.js
+++ src/App.js
@@ -21,12 +21,23 @@ function App() {
 
   var audio = new Audio();
 
+  //Read now playing
+  var internetradio = require("node-internet-radio");
+  var testStream = "";
+  internetradio.getStationInfo(testStream, function (error, station) {
+    console.log(station);
+  });
+
   const playMe = (e) => {
     // const myUrl = stations.filter((item) => e.target.innerHTML === item.name);
     console.log(e.target.id);
     audio.pause();
     audio = new Audio(e.target.id);
     audio.play();
+    testStream = e.target.id;
+    internetradio.getStationInfo(testStream, function (error, station) {
+      console.log(station);
+    });
   };
 
   const stopMe = () => {
@@ -43,12 +54,12 @@ function App() {
                 {item.name}
               </button>
               <button onClick={stopMe}>Stop</button>
-              <img
+              {/* <img
                 src={item.favicon}
                 width="50"
                 heigth="50"
                 alt={item.name}
-              ></img>
+              ></img> */}
               <p>Bitrate - {item.bitrate}</p>
               <a href={item.homepage} target="_blank">
                 Home page
