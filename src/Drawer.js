b1e3d20eb4490e5fb15e291a9771ce14d1b84357
rinatsharifullin <rinatsharifullin@flipdish.ie>
2021-01-01 14:02:33 +0000

    wider on mobile

--- src/Drawer.js
+++ src/Drawer.js
@@ -78,7 +78,8 @@ const useStyles = makeStyles((theme) => ({
   },
   rootCard: {
     display: "flex",
-    width: 250,
+    maxWidth: 500,
+    width: "100%",
     height: 100,
     justifyContent: "space-between",
     margin: 5,
